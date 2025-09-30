d3accfe5a99823242edcad8eb110afb11d49dec4
JeallyRabbit <karczewskiolek5@gmail.com>
2025-09-30 20:09:37 +0200

    asd

--- constants.js
+++ constants.js
@@ -441,7 +441,7 @@ const REACTION_STEP = 500
 const MIN_NUKER_RES_AMOUNT = 100 // minimum amount of energy or ghodium in storage/temrinal to consider transfering it to nuker
 const ROOM_DISTANCE_CALC_STEP = 1
 const DOCTOR_MIN_REMAINING_TIME=20
-const STRUCTURE_ONLY_ATTACK_POWER=1000
+const STRUCTURE_ONLY_ATTACK_POWER=500
 
 
 // Room attack
