6cfb82c13ceb06aad0b47a5059efc234719a8800
JeallyRabbit <karczewskiolek4@gmail.com>
2026-01-18 12:37:53 +0100

    asd

--- roomManager.js
+++ roomManager.js
@@ -748,9 +748,16 @@ Room.prototype.roomManager = function roomManager() {
             //global.heap.rooms[this.name].requiredRampartsRepairersPower = global.heap.rooms[this.name].rampartsEnergyNeedPerTick * C.RAMPARTS_REPAIRERS_FACTOR
            
             // The same formula as for workers but multiplied by 0.75 
-            global.heap.rooms[this.name].requiredRampartsRepairersPower=(Math.pow((this.storage.store[RESOURCE_ENERGY] / C.UPGRADE_FACTOR_1), 2) / C.UPGRADE_FACTOR_2)* C.RAMPARTS_REPAIRERS_FACTOR
+            if(this.storage!=undefined)
+            {
+                            global.heap.rooms[this.name].requiredRampartsRepairersPower=(Math.pow((this.storage.store[RESOURCE_ENERGY] / C.UPGRADE_FACTOR_1), 2) / C.UPGRADE_FACTOR_2)* C.RAMPARTS_REPAIRERS_FACTOR
+
+            }
+            else{
+
+            
             global.heap.rooms[this.name].requiredRampartsRepairersPower=Math.min(global.heap.rooms[this.name].requiredRampartsRepairersPower,global.heap.rooms[this.name].rampartsEnergyNeedPerTick)
-           
+            }
         }
         else {
             global.heap.rooms[this.name].requiredRampartsRepairersPower = 0
