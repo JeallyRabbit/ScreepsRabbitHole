852deb08161f40a6ee53dad7813c6711c428e20a
JeallyRabbit <karczewskiolek4@gmail.com>
2026-01-23 12:17:48 +0100

    asd

--- main.js
+++ main.js
@@ -27,6 +27,14 @@ Room.prototype.removeConstructionSites = function removeConstructionSites() {
   }
 }
 
+
+Room.prototype.resetLayout = function resetLayout() {
+  this.memory.finalRoomPlan = undefined
+  this.memory.finalBuildingList = []
+  this.memory.minRampartsAmount = 999999
+  this.memory.finishedPlanning = false
+}
+
 class attackRoom {
   constructor(roomName) {
 
@@ -362,7 +370,7 @@ module.exports.loop = function () {
             inAnyHarvestingRoom = true
             break
           }
-          else if( Memory.rooms[m].harvestingRooms!=undefined){
+          else if (Memory.rooms[m].harvestingRooms != undefined) {
             for (h of Memory.rooms[m].harvestingRooms) {
               if (Game.getObjectById(c).room != undefined && h.name == Game.getObjectById(c).room.name) {
                 inAnyHarvestingRoom = true
@@ -380,27 +388,27 @@ module.exports.loop = function () {
 
     //removing structures in dead rooms
     for (s in Game.structures) {
-        var inAliveRoom = false
-        for (m of Memory.mainRooms) {
-          if (Game.getObjectById(s).room != undefined && Game.getObjectById(s).room.name == m) {
-            inAliveRoom = true
-            break
-          }
-          else if(Memory.rooms[m].harvestingRooms!=undefined){
-            for (h of Memory.rooms[m].harvestingRooms) {
-              if (Game.getObjectById(s).room != undefined && h.name == Game.getObjectById(s).room.name) {
-                inAliveRoom = true
-                break
-              }
+      var inAliveRoom = false
+      for (m of Memory.mainRooms) {
+        if (Game.getObjectById(s).room != undefined && Game.getObjectById(s).room.name == m) {
+          inAliveRoom = true
+          break
+        }
+        else if (Memory.rooms[m].harvestingRooms != undefined) {
+          for (h of Memory.rooms[m].harvestingRooms) {
+            if (Game.getObjectById(s).room != undefined && h.name == Game.getObjectById(s).room.name) {
+              inAliveRoom = true
+              break
             }
           }
         }
-        if (inAliveRoom == false) {
-          Game.getObjectById(s).destroy()
-        }
-
       }
-    
+      if (inAliveRoom == false) {
+        Game.getObjectById(s).destroy()
+      }
+
+    }
+
 
 
   });
