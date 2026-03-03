0007ed4918b8745b81dd36787b4022a6dc2530fa
JeallyRabbit <karczewskiolek4@gmail.com>
2026-03-03 22:06:41 +0100

    asd

--- terminalManager.js
+++ terminalManager.js
@@ -215,7 +215,7 @@ Room.prototype.terminalManager = function terminalManager() {
     
     //Sharing energy to fastRclUpgrade
     if (Memory.fastRclUpgrade != undefined && Memory.fastRclUpgrade != this.name
-        && global.heap.rooms[this.name].building==true
+        && global.heap.rooms[this.name].building==false
         && this.storage!=undefined && this.storage.store[RESOURCE_ENERGY]>C.STORAGE_ENERGY_BOTTOM
         && this.terminal!=undefined && this.terminal.store[RESOURCE_ENERGY]>C.TERMINAL_BOTTOM_ENERGY
     ) {
