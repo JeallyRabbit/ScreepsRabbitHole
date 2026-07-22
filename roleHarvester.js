c31f2392a28afc7ff1e482cd849066a0bbdd6af4
JeallyRabbit <karczewskiolek4@gmail.com>
2026-07-22 22:46:23 +0200

    asd

--- roleHarvester.js
+++ roleHarvester.js
@@ -118,7 +118,7 @@ Creep.prototype.roleHarvester = function roleHarvester() {
             var energy_amount = this.store[RESOURCE_ENERGY]
             var transferResult = this.transfer(Game.getObjectById(this.memory.closestContainerId), RESOURCE_ENERGY)
             if (transferResult == ERR_NOT_IN_RANGE) {
-                this.travelTo(Game.getObjectById(this.memory.closestContainerId))
+                this.travelTo(Game.getObjectById(this.memory.closestContainerId),{ignoreCreeps:false})
             }
             else if (transferResult == OK) {
                 this.harvest(Game.getObjectById(this.memory.sourceId))
