39ff221afd5ea15a58d92352987a6434b4f78e7e
aleks.karczewski <aleks.karczewski@student.put.poznan.pl>
2026-08-13 12:21:37 +0200

    asd

--- roleCarrier.js
+++ roleCarrier.js
@@ -54,7 +54,7 @@ Creep.prototype.roleCarrier = function roleCarrier() {
 
         //dumb idea debugging
 
-        if(this.ticksToLive%(20+(getRandomInt(15)))==0)
+        if(this.ticksToLive%20==0)
         {
             delete this.memory._trav
         }
